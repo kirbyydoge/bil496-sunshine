774924c0b2c124132e745b0a0dc68036ccade8f3
elif gul <e.gul@etu.edu.tr>
2022-03-21 00:24:28 +0300

    add styling and mustache templates

---
+++ moodle/local/focusmode/version.php
@@ -0,0 +1,7 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2021051700.00;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2021051700.00;        // Requires this Moodle version.
+$plugin->component = 'local_focusmode';  // Full name of the plugin (used for diagnostics)
