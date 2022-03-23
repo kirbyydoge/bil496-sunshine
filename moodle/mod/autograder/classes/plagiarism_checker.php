7bfc3d41d90574d9dd8c3fe0fc24f0b754d423e2
Oguzhan Canpolat <aqwoguz@gmail.com>
2022-03-23 20:31:51 +0300

    Add initial template files for plagiarism checking.

---
+++ moodle/mod/autograder/classes/plagiarism_checker.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle Autograder Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    mod_autograder
+ * @author     Oğuzhan Canpolat
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class plagiarism_checker {
+
+    private const SRC_PATH = __DIR__ . "/../cache/src";
+    private const BIN_PATH = __DIR__ . "/../cache/bin";
+
+    public function check_plagiarism(int $assignmentid) {
+        //TODO: Move files to a cache directory and use MOSS to check plagiarism.
+        return $out_buffer;
+    }
+
+}
