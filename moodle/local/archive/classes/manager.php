4a90f07b375cbae5ce9d918376de6b2648c936e8
kirbyydoge <aqwoguz@gmail.com>
2022-04-17 15:47:01 +0300

    Fix multiple instances not being fetched and displayed.

--- moodle/local/archive/classes/manager.php
+++ moodle/local/archive/classes/manager.php
@@ -24,9 +24,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-use dml_exception;
-use stdClass;
-
 class manager {
 
     /** Insert the data into our database table.
