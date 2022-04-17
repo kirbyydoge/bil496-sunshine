9d6c31d2c7d34b2c94a2289f4a7e9853758cfe01
Serhat <60624888+srhtt@users.noreply.github.com>
2022-04-17 20:46:45 +0300

    obs frontend done

--- moodle/blocks/examsobs/block_examsobs.php
+++ moodle/blocks/examsobs/block_examsobs.php
@@ -8,7 +8,8 @@ class block_examsobs extends block_base {
 
 
     function get_content() {
-
+        $id          = optional_param('id', 0, PARAM_INT);
+        // echo "<script type='text/javascript'>alert('$id');</script>";
         if ($this->content !== NULL) {
             return $this->content;
         }
@@ -16,7 +17,17 @@ class block_examsobs extends block_base {
         $content = 'OBS activation are shown...';
         $this->content = new stdClass;
         $this->content->text = $content;
-        $this->content->footer = 'Click here to reach obs';
+        
+        $url=new moodle_url('/course/view.php', array('id' => $id, 'flag' => 3));
+        $url2=new moodle_url('/local/obsexamsreports/examsreport.php', array('id' => $id));
+        $this->content->text = html_writer::div(
+            html_writer::link($url2, "Click here to reach exam reports"),
+        );
+        $this->content->footer = html_writer::div(
+            
+            html_writer::link($url, "Click Here to start OBS"),
+        );
+      
         return $this->content;
     }
 }
